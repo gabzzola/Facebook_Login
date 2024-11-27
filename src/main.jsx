3f34035e4a6fa106b67d633b62405bfec6210817
gabzzola <gazzola.co@gmail.com>
2024-11-26 22:46:21 -0300

    Modificando o caminho do arquivo de estilos e alterando o componente renderizado de 'Login' para 'App'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Login from './pages/Login/index.jsx';
-import './pages/Login/index.css';
+import App from './App';
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>,
 );
